c45880e549f2378a10c9417c128b6cd7bc616db3
Suhair Zain <suhairzain11@gmail.com>
2017-01-02 19:02:12 +0530

    Made tech underline on hover

--- src/home/Tech.jsx
+++ src/home/Tech.jsx
@@ -4,22 +4,29 @@ import {
     COLOR_TEXT_IMPORTANT
 } from '../utils/colors'
 
+import Hover from '../core/Hover.jsx'
+
 const styles = {
-    root: {
+    default: {
         color: COLOR_TEXT_IMPORTANT,
         cursor: 'pointer',
-        fontSize: 'xx-large',
+        fontSize: 'xx-large'
+    },
+    hover: {
         textDecorationLine: 'underline',
         textDecorationStyle: 'dotted'
     }
 };
 
 const Tech = ({children}) => (
-    <span
-        title={`Show my projects made in ${children}`}
-        style={styles.root}>
-        {children}
-    </span>
+    <Hover
+        defaultStyle={styles.default}
+        hoverStyle={styles.hover}>
+        <span
+            title={`Show my projects made in ${children}`}>
+            {children}
+        </span>
+    </Hover>
 );
 
 export default Tech;
