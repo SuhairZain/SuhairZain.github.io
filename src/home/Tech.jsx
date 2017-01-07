7290e63985959c2a0d83fa88a1b87f7ce4aedfd6
Suhair Zain <suhairzain11@gmail.com>
2017-01-07 19:32:00 +0530

    Removed hover from Tech temporarily

--- src/home/Tech.jsx
+++ src/home/Tech.jsx
@@ -4,12 +4,9 @@ import {
     COLOR_TEXT_IMPORTANT
 } from '../utils/colors'
 
-import Hover from '../core/Hover.jsx'
-
 const styles = {
     default: {
         color: COLOR_TEXT_IMPORTANT,
-        cursor: 'pointer',
         fontSize: 'xx-large'
     },
     hover: {
@@ -19,14 +16,11 @@ const styles = {
 };
 
 const Tech = ({children}) => (
-    <Hover
-        defaultStyle={styles.default}
-        hoverStyle={styles.hover}>
-        <span
-            title={`Show my projects made in ${children}`}>
-            {children}
-        </span>
-    </Hover>
+    <span
+        style={styles.default}
+        title={`Show my projects made in ${children}`}>
+        {children}
+    </span>
 );
 
 export default Tech;
