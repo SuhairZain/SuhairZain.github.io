dbadecdcd68944002ae3cff32c09e8344c555bbe
Suhair Zain <suhair@languagezen.com>
2020-03-02 22:48:09 +0530

    Nuke everything to create new app with TS

--- src/core/Icon.jsx
+++
@@ -1,7 +0,0 @@
-import Inferno from 'inferno'
-
-const Icon = ({ height, src, width }) => (
-    <img height={height} src={src} width={width} />
-);
-
-export default Icon;
--- src/redux/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers, createStore } from 'redux';
-
-import ui from './ui';
-
-export default createStore(
-  combineReducers({
-    ui,
-  })
-);
