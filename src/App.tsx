c3b00c8a0b95b5571d8748199404789b0efe1964
Suhair Zain <suhair@languagezen.com>
2020-03-04 16:54:53 +0530

    Add under construction UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { PureComponent } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          This code is compiling. Please come back later.
+          <a
+            className="App-link"
+            href="https://www.xkcd.com/303/"
+            target="_blank"
+            rel="noopener noreferrer">
+            Inspiration >>
+          </a>
+        </header>
+        <footer className="App-footer"></footer>
+      </div>
+    );
+  }
 }
 
 export default App;
