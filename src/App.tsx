3bc4dd3fad6d3fe82a41bbbb680a8d494d3d9cf9
Suhair Zain <suhair@languagezen.com>
2020-07-19 14:01:24 +0530

    Change prettier config and prettify files

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,24 @@ import React, { PureComponent } from 'react';
 import './App.css';
 
 class App extends PureComponent {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          This code is compiling. Please come back later.
-          <a
-            className="App-link"
-            href="https://www.xkcd.com/303/"
-            target="_blank"
-            rel="noopener noreferrer">
-            Inspiration >>
-          </a>
-        </header>
-        <footer className="App-footer"></footer>
-        <footer className="App-footer Slanted"></footer>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    This code is compiling. Please come back later.
+                    <a
+                        className="App-link"
+                        href="https://www.xkcd.com/303/"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        Inspiration >>
+                    </a>
+                </header>
+                <footer className="App-footer"></footer>
+                <footer className="App-footer Slanted"></footer>
+            </div>
+        );
+    }
 }
 
 export default App;
