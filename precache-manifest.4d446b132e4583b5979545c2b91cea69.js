6d1d78279aa76ead53c0197c9d268063a766fb7e
Suhair Zain <suhair@languagezen.com>
2020-07-19 17:33:06 +0530

    Updates

---
+++ precache-manifest.4d446b132e4583b5979545c2b91cea69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec5965d21b57757f242f91e08d8ea1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "687ad3c4f514261dd49a",
+    "url": "/static/css/main.f6c93264.chunk.css"
+  },
+  {
+    "revision": "0649d84d66f0e64966a3",
+    "url": "/static/js/2.649846be.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.649846be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687ad3c4f514261dd49a",
+    "url": "/static/js/main.25b43f71.chunk.js"
+  },
+  {
+    "revision": "abc529edca4bd6a41ed5",
+    "url": "/static/js/runtime-main.3093b4de.js"
+  },
+  {
+    "revision": "980b9f32a3f482b7db688fa8df0488cd",
+    "url": "/static/media/Manrope-VariableFont_wght.980b9f32.ttf"
+  },
+  {
+    "revision": "f9baef8ac0d836e6486419e282e42336",
+    "url": "/static/media/Quicksand-VariableFont_wght.f9baef8a.ttf"
+  }
+]);
